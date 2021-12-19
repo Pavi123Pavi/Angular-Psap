8ea44bb4a9c8159a271b3c3b0e952cf96e0b6e50
Gopal Paul <gopal.paul@capgemini.com>
2021-12-20 03:17:10 +0530

    Update signup.component.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -15,56 +15,58 @@ export class SignupComponent implements OnInit {
     fullname : new FormControl('', Validators.required),
     mobno : new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
-    confPassword : new FormControl('',[Validators.required,this.confirmPasswordCheck])
+    // confPassword : new FormControl('',[Validators.required,this.confirmPasswordCheck])
   })
 
   constructor(private usersService: UsersService,  public router: Router) { }
 
   ngOnInit(): void {
     // this.checkLogin();
+
   }
   onSubmit(){
-    var loginJson = JSON.stringify(this.form.value);
-    this.usersService.signup(loginJson)
-    .subscribe((data => {
-      if(data == true){
+    // var loginJson = JSON.stringify(this.form.value);
+    // this.usersService.signup(loginJson)
+    // .subscribe((data => {
+    //   if(data == true){
       alert("Signup successful");
-      var jsonData = JSON.parse(loginJson);
-      this.storeLogin(jsonData['email']);
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(['/login']);
+      // var jsonData = JSON.parse(loginJson);
+      // this.storeLogin(jsonData['email']);
+      // this.router.navigate(['/dashboard']);
       
       }
-      else{
-        alert("User Already Exist"); 
-      }
-    }))
+      // else{
+      //   alert("User Already Exist"); 
+      // }
+    // }))
   }
 
-  storeLogin(email){
-    sessionStorage.setItem("email", email); 
-  }
+  // storeLogin(email){
+  //   sessionStorage.setItem("email", email); 
+  // }
 
-  checkLogin(){
-    if (sessionStorage.length != 0){
-      this.router.navigate(['/']);
-    }
-  }
+  // checkLogin(){
+  //   if (sessionStorage.length != 0){
+  //     this.router.navigate(['/']);
+  //   }
+  // }
 
-  confirmPasswordCheck(control: AbstractControl){
-    if (control && control.value !== null || control.value != undefined){
-      const cnfPassword = control.value;
-      const passControl = control.root.get('password')
+  // confirmPasswordCheck(control: AbstractControl){
+  //   if (control && control.value !== null || control.value != undefined){
+  //     const cnfPassword = control.value;
+  //     const passControl = control.root.get('password')
 
-      if (passControl){
-        const passValue = passControl.value;
-        if (passValue !== cnfPassword){
-          return {
-            isError: true
-          }
-        }
-      }
-    } 
-    return null
-  }
+  //     if (passControl){
+  //       const passValue = passControl.value;
+  //       if (passValue !== cnfPassword){
+  //         return {
+  //           isError: true
+  //         }
+  //       }
+  //     }
+  //   } 
+  //   return null
+  // }
 
-}
+// }
